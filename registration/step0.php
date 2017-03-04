4cc4e080294e009ca91eeff9698fd319a3b1201d
Dmitry <sheyko.dmitriy@gmail.com>
2017-03-04 13:58:01 +0200

    Add registration page

---
+++ registration/step0.php
@@ -0,0 +1,209 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta lang="ru">
+    <meta name="viewport"    content="width=device-width, initial-scale=1.0">
+    <meta name="googlebot"   content="noarchive">
+    <meta name="keywords"    content="грузовые перевозки, информационная система грузоперевозок, компании грузоперевозок, онлайн грузоперевозки, онлайн-сервис грузоперевозок, сайт грузоперевозок, сайт перевозки грузов">
+    <meta name="description" content="Грузоперевозки в Киеве, организация грузовых перевозок с помощью информационной системы Везём Всё. Каталог компаний">
+    <meta name="author" content="Dmytro Sheiko">    
+    <link rel="shortcut icon" href="assets/styles/images/favicon.ico">
+	<meta property="og:title" content="Везём Всё - онлайн-сервис по грузоперевозкам." />
+	<meta property="og:description" content="Узнайте стоимость своей перевозки и сэкономьте до 70%. Перевозим Всё, Везде, Всегда." />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content="index.html" />
+	<meta property="og:image" content="assets/images/other/social_logo.jpg" />
+	<meta property="og:site_name" content="vezemvse.com.ua" />
+	<meta itemprop="name" content="Везём Всё - онлайн-сервис по грузоперевозкам." />
+	<meta itemprop="description" content="Узнайте стоимость своей перевозки и сэкономьте до 70%. Перевозим Всё, Везде, Всегда." />
+	<meta itemprop="image" content="assets/images/other/social_logo.jpg" />
+	<meta itemprop="address" content="02000, Киев, Днепровская Наебержная 14Б, 1603" />
+	<meta itemprop="telephone" content="+38-095-029-29-03" />
+	<title>Доставка груза по Украине. Стоимость доставки груза - онлайн-аукцион сайт грузоперевозок по Украине «Везём Всё»</title>
+	<link type="text/css" rel="stylesheet" href="../listing/assets/main.css" media="screen" />
+	<link type="text/css" rel="stylesheet" href="assets/style.css" media="screen" />
+	<link type="text/css" rel="stylesheet" href="../assets/cache/201722/home_v42017222331.min.css" media="screen" />
+	<link type="text/css" rel="stylesheet" href="../assets/cache/2017228/common20172282416.min.css" media="screen"/>
+	<link type="text/css" rel="stylesheet" href="../assets/styles/pages/listing/represent_old.css" media="screen">
+	
+	<link type="text/css" rel="stylesheet" href="../assets/cache/201733/common2017332437.min.css" media="screen" />
+	<link type="text/css" rel="stylesheet" href="../assets/cache/201733/registration_common2017332437.min.css" media="screen" />
+	<link type="text/css" rel="stylesheet" href="../assets/cache/201733/registration_step02017332437.min.css" media="screen" />
+	<link type="text/css" rel="stylesheet" href="../assets/styles/v3/ipad-v3.css?v=11" media="only screen and (min-width: 768px) and (max-width: 979px)" />
+	<link type="text/css" rel="stylesheet" href="../assets/styles/v3/style_1024-v3.css?v=11" media="only screen and (min-width: 979px) and (max-width: 1199px)" />
+	<link type="text/css" rel="stylesheet" href="../assets/styles/v3/style_1280-v3.css?v=11" media="only screen and (min-width: 1254px)" />
+	<link type="text/css" rel="stylesheet" href="../assets/styles/v3/phone-v3.css?v=11" media="only screen and (max-width: 767px)" />
+
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+</head>
+<body style="background:#f9f8f3" class="body_below_header">
+	<div style="text-align:center">	
+		<header class="layout__header _fixed">
+		  <div class="layout__header-row _viewport_mobile">
+			<div class="layout__title">
+			  <img class="layout__logo" src="../assets/images/home_v4/logo-mobile.png" alt="Везет Всем — онлайн-сервис грузоперевозок" />
+			</div>
+		  </div>
+		  <div class="layout__header-row _viewport_desktop">
+			<div class="header">
+
+				
+			  <div class="header__content">
+				<div class="header__item header__title">
+				  <a href="/">
+					<img class="header__title-logo" src="../assets/images/home_v4/logo-white.png" alt="Сайт грузоперевозок «Везет Всем»">
+					<img class="header__title-logo _blue" src="../assets/images/home_v4/logo-blue.png" alt="Сайт грузоперевозок «Везет Всем»">
+				  </a>
+				  <a class="header__title-link" href="#">Онлайн-сервис перевозок</a>
+				</div>
+				<div class="header__support header__item">
+				  <span class="header__support-link contact x-open-support-box">Служба поддержки</span><br>
+				  <a class="header__support-phone" href="tel:0950292903">0 (095) 029-29-03</a><br/>
+				  Работаем круглосуточно
+				</div>
+			  </div>
+			</div>
+		  </div>
+		</header>	
+		
+		<section id="content" style="text-align:left">
+		<div class="x-auth-steps">
+    <form method="POST">
+        <div class="sub-content rff ">
+            <div id="x-registration-step0" class="container x-ac2">
+                <div class="row">
+                    <div class="span12 r2">
+                        <!--<div class="soc-blok">
+                            <h1 class="pustoi1">:</h1>
+
+                            <div class="sb1"><span class="pustoi1"></span><span>mail.ru</span></div>
+                            <div class="sb2"><span class="pustoi1"></span><span>yandex.ru</span></div>
+                            <div class="sb3"><span class="pustoi1"></span><span>google.com</span></div>
+                            <div class="sb4"><span class="pustoi1"></span><span>odnoklassniki.ru</span></div>
+                            <div class="sb5"><span class="pustoi1"></span><span>facebook.com</span></div>
+                            <div class="sb6"><span class="pustoi1"></span><span>vk.com</span></div>
+                        </div>-->
+                        <div class="anc1 mr15">
+                            <h1 class="pustoi1">Заполните все поля:</h1>
+                            <div class="s-itog11 x-auth-input-block">
+                                <div class="pustoi7">
+                                    <input type="text" class="pustoi1 x-auth-validate" placeholder="Имя" name="firstName">
+
+                                    <div class="pustoi3"></div>
+                                    <div class="pustoi4"></div>
+                                    <span class="pustoi6"></span>
+                                </div>
+                            </div>
+                            <div class="s-itog11 x-auth-input-block">
+                                <div class="pustoi7">
+                                    <input type="text" class="pustoi1 x-auth-validate" data-related-name="firstName" placeholder="Фамилия" name="lastName">
+
+                                    <div class="pustoi3"></div>
+                                    <div class="pustoi4"></div>
+                                    <span class="pustoi6"></span>
+                                </div>
+                            </div>
+                            <div class="s-itog11 varning x-auth-input-block">
+                                <div class="pustoi7">
+                                    <input type="text" data-tip="email" class="pustoi1 x-input x-auth-validate" placeholder="Электронная почта" name="email">
+                                    <span class="pustoi2"></span>
+                                    <div class="pustoi3"></div>
+                                    <div class="pustoi4 x-auth-status-correct"></div>
+                                    <span class="pustoi6"></span>
+                                </div>
+                                <div class="hover-img2 x-auth-email-base-tip x-tip" data-tip="email">
+                                    <div class="triangle"><div class="hover-img2__triangle-figure"></div></div>
+                                    <div class="hover-img__text">На этот адрес вам придёт подтверждающее письмо</div>
+                                </div>
+                                <div class="hover-img2 lhn x-auth-email-sugg-tip hidden"></div>
+                            </div>
+
+                            <div class="s-itog11 phone x-auth-input-block">
+                                <div class="pustoi7">
+                                    <div class="phone-block">
+                                        <span class="phone-prefix x-phone-code-show" style="margin-left:2px">+38</span>
+                                        <input type="text" style="width: 250px" class="x-auth-validate x-input" placeholder="Мобильный телефон" name="mobilePhone" data-tip="mobilePhone">
+                                    </div>
+                                    <div class="pustoi3"></div>
+                                    <div class="pustoi4"></div>
+                                    <span class="pustoi6"></span>
+                                </div>
+                                <div class="hover-img2 x-tip" data-tip="mobilePhone">
+                                    <div class="triangle"><div class="hover-img2__triangle-figure"></div></div>
+                                    <div class="hover-img__text">На указанный номер телефона придёт SMS с обязательным кодом подтверждения</div>
+                                </div>
+                            </div>
+
+
+                            <div class="s-itog11 x-auth-input-block">
+                                <div class="pustoi7">
+                                    <input type="hidden" class="x-ac-related x-ac-country x-country-id" name="country" data-ac-related-id="769827340" data-ac-related-name="country_id" value="1">
+
+                                    <input type="text" class="pustoi1 x-ac-related x-ac-input x-ac-city x-auth-validate x-input" placeholder="Город" data-ac-related-name="city_id" data-ac-related="769827340" name="city" data-tip="city" autocomplete="none">
+                                    <input type="hidden" class="x-ac-result-id" name="city_id">
+
+                                    <div class="pustoi3"></div>
+                                    <div class="pustoi4"></div>
+                                    <span class="pustoi6"></span>
+                                </div>
+                                <div class="hover-img2 x-tip" data-tip="city">
+                                    <div class="triangle"><div class="hover-img2__triangle-figure"></div></div>
+                                    <div class="hover-img__text">Укажите основной город вашей работы. Это необходимо для уведомлений по маршрутам</div>
+                                </div>
+                            </div>
+
+
+                            <div class="s-itog11 x-auth-input-block">
+                                <div class="pustoi7">
+                                    <span class="x-auth-show-password-input">
+                                        <input id="auth-password" type="text" class="pustoi1 x-auth-validate x-input" placeholder="Пароль" name="password" data-tip="password">
+                                    </span>
+                                    <div class="pustoi3"></div>
+                                    <div class="pustoi4"></div>
+                                    <span class="pustoi2"></span>
+                                    <span class="pustoi6"></span>
+                                </div>
+                                <div class="hover-img2 x-tip" data-tip="password">
+                                    <div class="triangle"><div class="hover-img2__triangle-figure"></div></div>
+                                    <div class="hover-img__text">Придумайте надёжный пароль для входа на сервис (не менее 6 символов)</div>
+                                </div>
+                            </div>
+                            <div class="s-itog11 x-auth-input-block" style="display: none;">
+                                <div class="pustoi7">
+                                    <span class="x-auth-show-confirm-password-input">
+                                        <input id="auth-confirm-password" type="password" class="pustoi1 x-auth-validate" data-related-name="password" placeholder="Подтвердите пароль" name="confirmPassword">
+                                    </span>
+                                    <span class="pustoi2"></span>
+
+                                    <div class="pustoi3"></div>
+                                    <div class="pustoi4"></div>
+                                    <span class="pustoi6"></span>
+                                </div>
+                            </div>
+                            <div class="linia6"></div>
+                            <div class="reg-but" style="margin-bottom:100px; padding-top:25px;">
+                                <div class="btn-submit-block" style="width:313px">
+                                    <button type="submit" class="btn-submit x-registration-button">Зарегистрироваться</button>
+                                </div>
+                                <!--<a target="_blank" href="../condition" class="sog">Пользовательское соглашение</a>-->
+                            </div>
+                        </div>
+                        <div class="anc2"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </form>
+</div>
+	</div></section>
+</body>
+<footer>
+	<article class="footer">
+		<div class="vv-container">
+			<div class="footer__copyright">
+				© 2017 Все права на&nbsp;<a class="footer__copyright-link" href="index.html">сайт грузоперевозок «vezemvse.com.ua»</a> принадлежат ООО&nbsp;«Везём Всё»
+			</div>
+		</div>
+	</article>
+</footer>
+</html>
