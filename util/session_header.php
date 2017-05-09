5ef2c3a31969a38919995ec749924f09fcf04ffd
Dmitry <sheyko.dmitriy@gmail.com>
2017-05-09 21:56:06 +0300

    Add freight search link to the session header

--- util/session_header.php
+++ util/session_header.php
@@ -12,9 +12,8 @@ if(isset($_SESSION['user_id'])){
 					<div id="unread_count" style="margin-left:5px; border-radius: 10px; background-color: #e84e3c; height:14px; padding: 2px 4px 1px; position: absolute; bottom: 10px; right: -10px; line-height: 1; font-size: 10px; display:none"></div>
 				</div>
 
-                <!--<a class="toolbar__balance toolbar__link" href="/accountv2/billing" title="Баланс">0</a>-->
-                <!--<a class="toolbar__faq toolbar__link" href="/help" title="Помощь для заказчика"></a>-->
                 <span class="toolbar__name">Здравствуйте, '.parseFirstName($_SESSION['name']).'</span>
+				'.($type=="0"?"<a class='toolbar__cabinet toolbar__link' href='poisk' style='text-decoration:none'>Поиск грузов</a>":"").'
                 <a class="toolbar__cabinet toolbar__link" href="/zaprosy" style="text-decoration:none">'.($type=="0"?"Мои предложения":"Мои запросы").'</a>
                 <a class="toolbar__logout toolbar__link" href="/auth/logout" style="text-decoration:none">Выйти<svg class="toolbar__logout-icon" xmlns="http://www.w3.org/2000/svg" viewBox="-296 385 24 23"><path d="M-279 400v4c0 1.7-1.3 3-3 3h-10c-1.7 0-3-1.1-3-3v-15c0-1.7 1.3-3 3-3h10c1.7 0 3 1.3 3 3v4h-2v-4c0-.5-.5-1-1-1h-10c-.6 0-1 .4-1 1v15c0 .6.5 1 1 1h10c.5 0 1-.5 1-1v-4h2z"></path><path d="M-273 396.5l-5-3.5v2.5h-10v2h10v2.5"></path></svg></a>
 			</div>
