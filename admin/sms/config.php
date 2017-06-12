76070ff0daeb0dd7320943e9a9dfdef0f8a559d3
Dmitry <sheyko.dmitriy@gmail.com>
2017-06-12 12:06:56 +0300

    Change SMS API key

--- admin/sms/config.php
+++ admin/sms/config.php
@@ -1,7 +1,7 @@
 <?php
 include 'APISMS.php';
 //public & private ключи, можно установить на странице https://atompark.com/members/settings#sms
-$sms_key_private='51815e99cfd57648f48e9713c20cd70e';
-$sms_key_public='5c767d14ba78a23edcd3954cfe7a548c';
+$sms_key_private='db71af921c67a872a00e6043c927c799';
+$sms_key_public='3b88e0941c642012d12defa887307b81';
 $URL_GAREWAY='http://atompark.com/api/sms/';
-$testMode=true;
+$testMode=false;
